4d2892c5758a0568dd572302e0bcde85ec8f018d
Sandeep prajapati <61901052+sandeep-sparrow@users.noreply.github.com>
2021-04-21 00:32:51 +0530

    Added Application Icon .ico

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -18,6 +18,9 @@ namespace HelloWorld
             double d1 = 3.5;
             double d2 = 1.337;
             double sumd = d1 + d2;
+            // value data type int, float, double, char, bool
+            // reference data types stores only the memmory location of data type
+            // string, class, Array
             Console.WriteLine("Sum of double type {0} + {1} = {2} ", d1, d2, sumd);
             Console.ReadLine();
         }
