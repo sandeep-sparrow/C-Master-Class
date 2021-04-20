34d6ccb5062121f6324d4ae358462ecd49059b04
Sandeep prajapati <61901052+sandeep-sparrow@users.noreply.github.com>
2021-04-21 00:02:39 +0530

    Data types

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -11,6 +11,14 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int num1 = 13;
+            int num2 = 13;
+            int sum = num1 + num2;
+            Console.WriteLine("Sum of int types {0} + {1} = {2} ", num1, num2, sum);
+            double d1 = 3.5;
+            double d2 = 1.337;
+            double sumd = d1 + d2;
+            Console.WriteLine("Sum of double type {0} + {1} = {2} ", d1, d2, sumd);
             Console.ReadLine();
         }
     }
