93ef01bd9b6a362e53721b1616731a3aee6cb774
Sandeep prajapati <61901052+sandeep-sparrow@users.noreply.github.com>
2021-04-21 17:30:12 +0530

    classes in C#, Array in C#

---
+++ HelloWorld/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    partial class Program
+    {
+        public class Person
+        {
+            public string FirstName;
+            public string SecondName;
+
+            public void Introduce()
+            {
+                Console.Beep();
+                Console.WriteLine("Hi, My name is {0} {1}", FirstName, SecondName);
+            }
+        }
+        
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HelloWorld.Math;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,56 +7,37 @@ using System.Threading.Tasks;
 
 namespace HelloWorld
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
-            // value data type int, float, double, char, bool
-            // reference data types stores only the memmory location of data type
-            // string, class, Array
+            var person = new Person();
+            person.FirstName = "Mistu";
+            person.SecondName = "Pratikbhai Prajapati";
+            person.Introduce();
 
-            int num1 = 13;
-            int num2 = 13;
-            int sum = num1 + num2;
-            Console.WriteLine("Sum of int types {0} + {1} = {2} ", num1, num2, sum);
+            Calculator calculator = new Calculator();
+            int result = calculator.Add(2, 2);
+            Console.WriteLine("Add method Result is: " + result);
 
-            double d1 = 3.5;
-            double d2 = 1.337;
-            double sumd = d1 + d2;
-            Console.WriteLine("Sum of double type {0} + {1} = {2} ", d1, d2, sumd);
+            // int[] Numbers = new int[3];
+            var Numbers = new int[3];
+            Numbers[0] = 10;
+            Console.WriteLine(Numbers[0]);
+            Console.WriteLine(Numbers[1]);
+            Console.WriteLine(Numbers[2]);
 
-            // type casting
-            double myDouble = 13.27;
-            int myInt;
-            myInt = (int)myDouble;      // Explicit conversion
-            Console.WriteLine(myInt);
+            var Flags = new bool[3];
+            Flags[0] = true;
+            Console.WriteLine(Flags[0]);
+            Console.WriteLine(Flags[1]);
+            Console.WriteLine(Flags[2]);
 
-            int num = 1234567856;
-            long bigNum = num;          // implicit conversion
-            Console.WriteLine(bigNum);
+            Console.Read();
 
-            // type conversion
-            string myString = myDouble.ToString();
-            Console.WriteLine(myString);
-
-            float myFloat = 12.12f;
-            string myFloatString = myFloat.ToString();
-            Console.WriteLine(myFloatString);
-
-            // String Parsing
-            string str1 = "15";
-            string str2 = "15";
-            var sum1 = str1 + str2;
-            Console.WriteLine("Sum: {0} + {1} = {2}", str1, str2, sum1);
-
-            int int1 = Int32.Parse(str1);
-            int int2 = Int32.Parse(str2);
-            var sum2 = int1 + int2;
-            Console.WriteLine("Sum: {0} + {1} = {2}", int1, int2, sum2);
-
-            Console.ReadLine();
         }
+        
     }
 }
